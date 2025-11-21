a862fb24ec9a3a950a38e7e56d4bbfa87edc642e
Oluwatobi <oldwhisper98@icloud.com>
2025-11-20 23:26:35 -0500

    get number of tables

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,14 @@ fn main() -> Result<()> {
             // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
             #[allow(unused_variables)]
             let page_size = u16::from_be_bytes([header[16], header[17]]);
+            let tbl_count = u16::from_be_bytes([header[18], header[19]]);
 
             // You can use print statements as follows for debugging, they'll be visible when running tests.
             eprintln!("Logs from your program will appear here!");
 
             // TODO: Uncomment the code below to pass the first stage
             println!("database page size: {}", page_size);
+            println!("number of tables: {}", tbl_count);
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
